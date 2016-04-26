32404fcdef6c06bbff44be5ebc7dd07b60cc567c
Blanca Azucena Lopez <blanca.lopez@data.mx>
2016-04-26 12:19:12 -0500

    adding login, signup and user profile views and controllers

---
+++ src/app/users/profile.controller.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('bonappettit')
+    .controller("ProfileController", ProfileController);
+
+  /** @ngInject */
+  function ProfileController(){
+    
+  }
+})();
