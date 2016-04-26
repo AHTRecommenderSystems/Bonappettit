6a7e19ddd69bba5b08b0194095c83f118261af9b
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2016-04-25 23:57:06 -0500

    Add states to route views

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,36 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/users/login.html',
+        controller: 'loginController',
+        controllerAs: 'login'
+      })
+      .state('register', {
+        url: '/signup',
+        templateUrl: 'app/users/signup.html',
+        controller: 'signupControlelr',
+        controllerAs: 'signup'
+      })
+      .state('createDish', {
+        url: '/dish/create',
+        templateUrl: 'app/dishes/createDish.html',
+        controller: 'createDishController',
+        controllerAs: 'createDish'
+      })
+      .state('dishInfo', {
+        url: '/dish',
+        templateUrl: 'app/dishes/info.html',
+        controller: 'dishInfoController',
+        controllerAs: 'dishInfo'
+      })
+      .state('userProfile', {
+        url: '/profile',
+        templateUrl: 'app/users/profile.html',
+        controller: 'profileController',
+        controllerAs: 'profile'
       });
 
     $urlRouterProvider.otherwise('/');
--- src/main/java/com/aht/dao/country/CountryDAO.java
+++
@@ -1,8 +0,0 @@
-package com.aht.dao.country;
-
-import java.util.LinkedList;
-import com.aht.model.Country;
-
-public interface CountryDAO {
-	public LinkedList<Country> retrieveAll();
-}
